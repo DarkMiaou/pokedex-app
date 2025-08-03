580d55264c8028e17e65b4608ad697e5c92ca940
Mathieu Morales <mathieumorales1612@gmail.com>
2025-08-03 21:35:05 +0200

    feat: archi nd api.js

---
+++ pokedex-app/services/api.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const API_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+export const getPokemons = async (offset = 0, limit = 20) => {
+  const res = await axios.get(`${API_URL}?offset=${offset}&limit=${limit}`);
+  return res.data;
+};
